9d74c0360066208b28ef644a4966aba6d292035e
pkuzhangjiashuo@gmail.com <1700012848@pku.edu.cn>
2020-05-12 21:40:43 +0800

    Update Kernel2IRVisitor.h

--- kernel/main.cpp
+++ kernel/main.cpp
@@ -3,15 +3,16 @@
 #include "antlr4-runtime/kernelLexer.h"
 #include "antlr4-runtime/kernelParser.h"
 #include "Kernel2IRVisitor.h"
-
+#include "IRPrinter.h"
+#include "type.h"
 #include "IR.h"
 using namespace std;
 using namespace antlr4;
 using namespace Boost::Internal;
+
 int main(int argc, const char* argv[]) {
     std::ifstream stream;
     stream.open("input.kernel");
-    
     ANTLRInputStream input(stream);
     kernelLexer lexer(&input);
     CommonTokenStream tokens(&lexer);
@@ -22,6 +23,9 @@ int main(int argc, const char* argv[]) {
    // simpleVisitor visitor;
     //antlrcpp::Any p=visitor.visitStart(tree);
     Kernel2IRVisitor visitor;
-    antlrcpp::Any p=visitor.visit(tree);
+    auto p=visitor.visit(tree).as<vector<Stmt>>();
+    IRPrinter printer;
+   // Move a;
+    //printer.visit(a);
     return 0;
 }
---
+++ kernel/test/CMakeFiles/gemm.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/gemm.dir/gemm.cc.o"
+  "gemm"
+  "gemm.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/gemm.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
