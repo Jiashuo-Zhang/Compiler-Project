473decb1762a83b27b37d4ea374714c6f1773017
Quack <quack8102@gmail.com>
2020-05-17 16:03:38 +0800

    lab repo created

--- project1/kernels/kernel_case7.cc
+++ project1/kernels/kernel_case7.cc
@@ -1,3 +1,28 @@
 #include "../run.h"
 
-void kernel_case7(float (&A)[32][16], float (&B)[16][32]) {}
+void kernel_case7(float (&A)[32][16], float (&B)[16][32])  {
+ float temp_B[16][32];
+ for (int i = 0; i < 0 + 16; i++) {
+   for (int j = 0; j < 0 + 32; j++) {
+     if (j >= 0 && j < 32) {
+       if (i >= 0 && i < 16) {
+         temp_B[i][j] = 0;
+         if (i >= 0 && i < 16) {
+           if (j >= 0 && j < 32) {
+             temp_B[i][j] = temp_B[i][j] + A[j][i];
+           }
+         }
+       }
+     }
+   }
+ }
+ for (int i = 0; i < 0 + 16; i++) {
+   for (int j = 0; j < 0 + 32; j++) {
+     if (j >= 0 && j < 32) {
+       if (i >= 0 && i < 16) {
+         B[i][j] = temp_B[i][j];
+       }
+     }
+   }
+ }
+}
